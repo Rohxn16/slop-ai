a2c9fb73dc7c62cbf96761f3fdd9920024e441ec
Rohxn16 <dev.rohxn16@gmail.com>
2026-05-08 22:53:07 +0530

    made minor changes in the ui. Ui now displays generated pov. Also allows user to regenerate the pov if they decide to.

--- ui/ui.py
+++ ui/ui.py
@@ -1,11 +1,21 @@
 import streamlit as st
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from pov_generator.pov import POV
+from pov_generator.generate import ChatPOVGenerator
+
+# init objects
+pov = POV()
+chat_pov_generator = ChatPOVGenerator(model="llama3.2:3b", system_prompt=pov.SYSTEM_PROMPT_1)
 
 st.write("# Welcome to Slop.AI !")
 st.write('### Drop your story and generate your production ready video.')
 
 story: str = st.text_area("Enter your story / context here. Do not exceed 2000 characters", height=400)
 if st.button('Generate Script'):
-    print(story)
+    with st.spinner('Generating your POV story...'):
+        pov_story = pov.generate_pov_from_text(story, chat_pov_generator)
+    st.write("### Generated POV Story:")
+    st.write(pov_story)
