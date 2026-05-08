2473503c09924e2808d99e8ff6510ed525d9d908
Rohxn16 <dev.rohxn16@gmail.com>
2026-05-08 22:53:28 +0530

    made minor changes in the ui. Ui now displays generated pov. Also allows user to regenerate the pov if they decide to.

--- ui/ui.py
+++ ui/ui.py
@@ -14,8 +14,19 @@ st.write("# Welcome to Slop.AI !")
 st.write('### Drop your story and generate your production ready video.')
 
 story: str = st.text_area("Enter your story / context here. Do not exceed 2000 characters", height=400)
-if st.button('Generate Script'):
+
+col1, col2 = st.columns([1, 4])
+with col1:
+    generate_clicked = st.button('Generate Script')
+with col2:
+    regenerate_clicked = False
+    if 'pov_story' in st.session_state:
+        regenerate_clicked = st.button('Regenerate Script')
+
+if generate_clicked or regenerate_clicked:
     with st.spinner('Generating your POV story...'):
-        pov_story = pov.generate_pov_from_text(story, chat_pov_generator)
+        st.session_state.pov_story = pov.generate_pov_from_text(story, chat_pov_generator)
+
+if 'pov_story' in st.session_state:
     st.write("### Generated POV Story:")
-    st.write(pov_story)
+    st.write(st.session_state.pov_story)
